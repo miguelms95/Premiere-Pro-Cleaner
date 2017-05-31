47ddb250b86bd21b8b1070b64384ed206f2cf16c
miguelms95 <miguelms.ovi@gmail.com>
2017-05-31 14:06:16 +0200

    premiere cleaner v1.0
    Clean the proyects before backup it or move anywhere. Delete unnecesary
    files like previsualization files generated (.CFA, .PEK, rendered .AVI)

---
+++ premiereCleaner.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+from os import listdir
+import glob
+import fnmatch
+
+
+os.chdir('D:\cap17 26092016\proyecto 17\\')
+
+contadorPEK = 0
+contadorCFA = 0
+contadorAVI = 0
+
+TRACEBACKPRINT = True
+DELETING = True
+
+for root, dirs, files in os.walk('D:\cap17 26092016\proyecto 17\\'):
+    for directorio in dirs:
+        if directorio.endswith(".PRV"):
+            print directorio
+    for file in files:
+        if file.endswith(".pek"):
+            contadorPEK += 1
+            if TRACEBACKPRINT:
+                print file
+            if DELETING:
+                os.remove(os.path.join(root, file))
+
+        if file.endswith(".cfa"):
+            contadorCFA += 1
+            if TRACEBACKPRINT:
+                print file
+            if DELETING:
+                os.remove(os.path.join(root, file))
+        if fnmatch.fnmatch(file, 'Rendered - *.AVI'):
+            contadorAVI += 1
+            if TRACEBACKPRINT:
+                print file
+            if DELETING:
+                os.remove(os.path.join(root, file))
+
+print '\n\n #### DATOS ####'
+print str(contadorAVI) + " archivos AVI eliminados"
+print str(contadorPEK) + " archivos PEK eliminados"
+print str(contadorCFA) + " archivos CFA eliminados"
+
+raw_input("\nPresiona una tecla para cerrar...")
+exit(0)
+
+'''
+listFiles = glob.glob('*.pek')
+for file in listFiles:
+    print file
+
+
+for item in test:
+    if item.endswith(".pek"):
+        print os.path.join( directory, item )
+        os.remove( os.path.join( directory, item ) )
+'''
